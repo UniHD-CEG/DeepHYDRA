2a0723e148e916257b71a63a7ca6985c4a90b0bc
Kevin Franz Stehle <kevin.franz.stehle@cern.ch>
2023-05-09 01:43:54 +0200

    Final commit before ECAI 23 submission

--- detection_combined/benchmark/informers/run_informers_combined.sh
+++ detection_combined/benchmark/informers/run_informers_combined.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 # Predetermined randomized seeds to ensure replicability
-#declare -a seeds=(7 129 28 192 85 148 142 30 78 33)
-declare -a seeds=(148 142 30 78 33)
+declare -a seeds=(7 129 28 192 85 148 142 30 78 33)
+
 for seed in ${seeds[@]}
 do
     python3 informers.py --seed ${seed} --model 'Informer-MSE' --checkpoint-dir "../../../transformer_based_detection/informers/checkpoints/hlt_mse_Scale_0.8_1.0_Scale_APP_0.8_1.0_0.01_0.05_0.05_rel_size_1.0_ratio_0.25_seed_${seed}"
